09ea2b91740c84b47f353d24aa65a13354a92df7
ThiagoDS14 <63246287+ThiagoDS14@users.noreply.github.com>
2020-04-06 14:11:11 -0300

    Update atividade1.java

--- atividade1.java
+++ atividade1.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class MyClass {
-    public static void main(String args[]) {
-     int A=3;
-     int B=9;
-
-      System.out.println("PROD = " + (A*B));
+    public static void main(String args[]) {  
+      int n1;
+      int n2;
+      
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.println("Digite o primeiro número: ");
+      n1 = sc.nextInt();
+      
+      System.out.println("Digite o segundo número: ");
+      n2 = sc.nextInt();
+      
+      int Prod = n1 * n2;
+      
+      System.out.println("PROD = " + Prod );
     }
 }
